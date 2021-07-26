f5bc99e86ddd8e7692748828ac5939499be1a8bf
Norbert Kruk <norbert.kruk@codibly.com>
2021-07-26 23:53:56 +0200

    fix(isNotEmptyString): Added empty value handle

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,2 +1,2 @@
-export const isNotEmptyString = (value: string): boolean => !!value.trim();
+export const isNotEmptyString = (value?: string): boolean => !!value?.trim();
 export const getArrayWithUniqueValues = <T>(array: T[]): T[] => [...new Set(array)];
